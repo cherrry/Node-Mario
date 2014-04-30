fb1bb50415d59139fc9db6e68f40267c881277b0
Cherry NG <cherry92@gmail.com>
2014-04-30 15:47:33 +0800

    socket.io (start)

---
+++ server.js
@@ -0,0 +1,21 @@
+var port = process.env.PORT || 8080;
+var server = require('http').createServer();
+server.listen(port);
+console.log(port);
+
+var io = require('socket.io').listen(server);
+io.configure('origins', 'cherrry.github.io');
+
+var char_list = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz";
+function random_string(length) {
+  ret = '';
+  for (i = 0; i < length; i++) {
+    ret += char_list.charAt(Math.floor(Math.random() * 60));
+  }
+  return ret;
+}
+
+var guests = new Object();
+io.sockets.on('connection', function(socket) {
+  socket.emit('message', { message: 'Hello' });
+});
