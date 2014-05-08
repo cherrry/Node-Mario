00ccedfa3636fc6501ade7fcf229ec68bdba63da
Cherry NG <cherry92@gmail.com>
2014-05-08 15:56:46 +0800

    adjust game map

--- world/w1/s1.js
+++ world/w1/s1.js
@@ -18,6 +18,9 @@ module.exports = (function () {
   Stage.solids.push({ x: 14, y: 12, type: 'Tube', attr: { frame: 1 } });
   Stage.solids.push({ x: 14, y: 11, type: 'Tube', attr: { frame: 0 } });
 
+  Stage.solids.push({ x: 22, y: 13, type: 'Tube', attr: { frame: 1 } });
+  Stage.solids.push({ x: 22, y: 12, type: 'Tube', attr: { frame: 0 } });
+
   for (var i = 7; i < 12; i++) {
     Stage.solids.push({ x: i, y: Stage.height - 7, type: 'Brick', attr: { isEmpty: 1 } });
   }
@@ -26,8 +29,12 @@ module.exports = (function () {
   Stage.collectibles = Array();
 
   Stage.collectibles.push({ x: 9, y: 13, type: 'Mushroom', collidable: true, attr: { id: 'mushroom_0', color: 'brown', state: 'alive' } });
+  Stage.collectibles.push({ x: 16, y: 13, type: 'Mushroom', collidable: true, attr: { id: 'mushroom_1', color: 'brown', state: 'alive' } });
+  Stage.collectibles.push({ x: 18, y: 13, type: 'Mushroom', collidable: true, attr: { id: 'mushroom_2', color: 'brown', state: 'alive' } });
+
+
   Stage.collectibles.push({ x: Stage.width - 3, y: Stage.height -9, type: 'Flagpole', collidable: false, attr: { id: 'flagpole' } });
-  Stage.collectibles.push({ x: 20, y: 13, type: 'PowerUp', collidable: false, attr: { id: 'powerup_0', type: 'grow' } });
+  Stage.collectibles.push({ x: 23, y: 13, type: 'PowerUp', collidable: false, attr: { id: 'powerup_0', type: 'grow' } });
 
   for (var i = 7; i < 12; i++) {
     Stage.collectibles.push({ x: i, y: Stage.height - 8, type: 'Coin', collidable: false, attr: { id: 'coin_' + (i - 7) } });
