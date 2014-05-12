6bbaef5e02e6703dbe95afb5bf744c1c5989a527
huikaihoo <huikaihoo@gmail.com>
2014-05-12 15:24:56 +0800

    Add water Tiles

--- world/w1/s2.js
+++ world/w1/s2.js
@@ -8,12 +8,27 @@ module.exports = (function () {
   Stage.solids = Array();
 
   for (var i = 0; i < Stage.width; i++) {
-    Stage.solids.push({ x: i, y: Stage.height - 1, type: 'Land', attr: {} });
+    if (i < Stage.width -20){
+      Stage.solids.push({ x: i, y: Stage.height - 1, type: 'Land', attr: {} });
+    }else{
+      //Stage.solids.push({ x: i, y: Stage.height - 1, type: 'Land', attr: {} });
+    }
+  }
+  for (var i = 0; i < Stage.width-20; i++) {
+    Stage.solids.push({ x: i, y: Stage.height - 2, type: 'Land', attr: {} });
   }
 
   Stage.collectibles = Array();
 
   Stage.collectibles.push({ x: Stage.width - 3, y: Stage.height -9, type: 'Flagpole', collidable: false, attr: { id: 'flagpole' } });
+
+  for (var i = 0; i < Stage.width; i++) {
+    if (i < Stage.width -20){
+      //Stage.solids.push({ x: i, y: Stage.height - 1, type: 'Land', attr: {} });
+    }else{
+      Stage.collectibles.push({ x: i, y: Stage.height - 1, type: 'Water', collidable: true, attr: {} });
+    }
+  }
 
   return Stage;
 })();
