6b935031f483bee445f0ca85073e8919fa09f340
Cherry NG <cherry92@gmail.com>
2014-05-14 16:58:37 +0800

    choose game world

--- world/w2.js
+++ world/w2.js
@@ -1,8 +1,8 @@
 module.exports = (function () {
   var World = Array();
 
-  for (var i=0; i<5; i++){
-  	World[i] = require('./w1/s' + (i+1) );
+  for (var i = 0; i < 5; i++) {
+    World[i] = require('./w1/s' + (5 - i) );
   }
 
   return World;
